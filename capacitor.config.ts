27a9185e3bf762fa92944b5959cda6d9bb554eeb
RAYAN LOPES AMERICO <rayan.americo@alunos.sc.senac.br>
2025-05-07 10:47:23 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'login-app',
+  webDir: 'www'
+};
+
+export default config;
